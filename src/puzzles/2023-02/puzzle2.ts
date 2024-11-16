41e5e7be70a2b44f1f94c2c7c83390df97c7a794
Nick Rountree <theforestgardener@gmail.com>
2024-11-17 10:43:08 +1300

    Puzzle 2023-02b

--- src/puzzles/2023-02/puzzle2.ts
+++ src/puzzles/2023-02/puzzle2.ts
@@ -3,8 +3,32 @@ import data from './input.txt?raw'
 export function answer() {
   let total = 0
 
-  console.log(`${data.split('\n').length} lines to process`)
-
-  //
+  data.split('\n').forEach((line) => {
+    let maxRed = 0
+    let maxGreen = 0
+    let maxBlue = 0
+    const gameNumber = parseInt(line.match(/Game (\d+)/)![1])
+    // Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+    const lineAfterColon = line.split(': ')[1]
+    const rounds = lineAfterColon.split('; ')
+    for (const round of rounds) {
+      const colors = round.split(', ')
+      for (const color of colors) {
+        const [count, colorName] = color.split(' ')
+        if (colorName === 'red') {
+          maxRed = Math.max(maxRed, parseInt(count))
+        }
+        if (colorName === 'green') {
+          maxGreen = Math.max(maxGreen, parseInt(count))
+        }
+        if (colorName === 'blue') {
+          maxBlue = Math.max(maxBlue, parseInt(count))
+        }
+      }
+    }
+    const gamePower = maxRed * maxGreen * maxBlue
+    total += gamePower
+  })
+  // 66681
   return total
 }
