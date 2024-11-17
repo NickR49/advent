70fd7541060d680ed54e15d3321cb9f4069cb923
Nick Rountree <theforestgardener@gmail.com>
2024-11-17 21:51:23 +1300

    Puzzle 2023-03b

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -4,7 +4,12 @@ interface Props {
 
 export default function Result(props: Props) {
   return (
-    <button class='w-[200px] rounded-full bg-gray-100 border-2 border-gray-300 focus:border-gray-400 active:border-gray-400 px-[2rem] py-[1rem]'>
+    <button
+      onClick={() =>
+        navigator.clipboard.writeText(props.result?.toString() ?? '')
+      }
+      class='w-[200px] rounded-full bg-gray-100 border-2 border-gray-300 focus:border-gray-400 active:border-gray-400 px-[2rem] py-[1rem]'
+    >
       {props.result ?? '-'}
     </button>
   )
--- src/puzzles/2023-03/puzzle2.ts
+++ src/puzzles/2023-03/puzzle2.ts
@@ -1,8 +1,116 @@
 import data from './input.txt?raw'
 
+interface SchematicAster {
+  x: number
+  y: number
+  values: number[]
+}
+
+function gobbleLeft(line: string, x: number): string {
+  let result = ''
+  for (let i = x - 1; i >= 0; i--) {
+    if (line.charAt(i).match(/[0-9]/)) {
+      result = line.charAt(i) + result
+    } else {
+      break
+    }
+  }
+  return result
+}
+
+function gobbleRight(line: string, x: number): string {
+  let result = ''
+  for (let i = x + 1; i < line.length; i++) {
+    if (line.charAt(i).match(/[0-9]/)) {
+      result = result + line.charAt(i)
+    } else {
+      break
+    }
+  }
+  return result
+}
+
+function gobbleNumbers(line: string, x: number): string[] {
+  const results: string[] = []
+  if (line.charAt(x).match(/[0-9]/)) {
+    const left = gobbleLeft(line, x)
+    const right = gobbleRight(line, x)
+    results.push(left + line.charAt(x) + right)
+  } else {
+    if (x > 0 && line.charAt(x - 1).match(/[0-9]/)) {
+      results.push(gobbleLeft(line, x))
+    }
+    if (x < line.length - 1 && line.charAt(x + 1).match(/[0-9]/)) {
+      results.push(gobbleRight(line, x))
+    }
+  }
+  return results
+}
+
+const numberRegex = /[0-9]+/
+
+function findAdjacentNumbers(
+  allLines: string[],
+  aster: SchematicAster,
+): SchematicAster {
+  // Check the line above
+  if (aster.y > 0) {
+    const lineAbove = allLines[aster.y - 1]
+    const results = gobbleNumbers(lineAbove, aster.x)
+    results.forEach((result) => aster.values.push(parseInt(result, 10)))
+  }
+  // Check the line below
+  if (aster.y < allLines.length - 1) {
+    const lineBelow = allLines[aster.y + 1]
+    const results = gobbleNumbers(lineBelow, aster.x)
+    results.forEach((result) => aster.values.push(parseInt(result, 10)))
+  }
+  const currentLine = allLines[aster.y]
+  // Check to the left
+  if (aster.x > 0) {
+    const result = gobbleLeft(currentLine, aster.x)
+    if (result) {
+      aster.values.push(parseInt(result, 10))
+    }
+  }
+  // Check to the right
+  if (aster.x < currentLine.length - 1) {
+    const result = gobbleRight(currentLine, aster.x)
+    if (result) {
+      aster.values.push(parseInt(result, 10))
+    }
+  }
+
+  return aster
+}
+
 export function answer() {
   let total = 0
+  const asters: SchematicAster[] = []
 
-  //
+  // Find all asters in the input and store in an array with their coordinates
+  data.split('\n').forEach((line, y) => {
+    const matches = line.matchAll(/\*/g)
+    for (const match of matches) {
+      asters.push({
+        x: match.index,
+        y,
+        values: [],
+      })
+    }
+  })
+
+  // Iterate through each aster and check for adjacent numbers
+  const allLines: string[] = data.split('\n')
+  asters
+    .map((aster) => findAdjacentNumbers(allLines, aster))
+    .forEach((aster) => {
+      console.log(`Aster at ${aster.y}, ${aster.x} has values: ${aster.values}`)
+      if (aster.values.length === 2) {
+        total += aster.values[0] * aster.values[1]
+      }
+    })
+
+  // 91622824
   return total
 }
