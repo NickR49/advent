cd30e4b11a5c6dac5d647907f2c219636da2d25e
Nick Rountree <theforestgardener@gmail.com>
2024-12-02 12:50:58 +1300

    Puzzle 2024-01b

--- src/puzzles/2024-01/puzzle2.ts
+++ src/puzzles/2024-01/puzzle2.ts
@@ -1,10 +1,26 @@
-import data from './input.txt?raw'
+import data from './sample.txt?raw'
 
 export function answer() {
-  let total = 0
-
   console.log(`${data.split('\n').length} lines to process`)
 
-  //
+  // Extract the two lists
+  const list1 = data
+    .split('\n')
+    .map((line) => parseInt(line.split('   ')[0]))
+    .sort((a, b) => a - b)
+  const list2 = data
+    .split('\n')
+    .map((line) => parseInt(line.split('   ')[1]))
+    .sort((a, b) => a - b)
+
+  let total = 0
+
+  list1.forEach((item) => {
+    // Find number of occurrences of this number in right list
+    const count = list2.filter((item2) => item2 === item).length
+    total += item * count
+  })
+
+  // 21790168
   return total
 }
