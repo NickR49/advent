0c085d5dd6f4cd0334933432a054d5a1a87bc2ba
Nick Rountree <theforestgardener@gmail.com>
2024-12-03 01:39:11 +1300

    Puzzle 2024-02a, b - refactor

--- src/puzzles/2024-02/puzzle1.ts
+++ src/puzzles/2024-02/puzzle1.ts
@@ -1,33 +1,45 @@
 import { isEqual } from 'lodash'
 import data from './input.txt?raw'
 
+function extractReportLevels(report: string): number[] {
+  return report.split(' ').map((level) => parseInt(level, 10))
+}
+
+function areLevelsSorted(levels: number[]): boolean {
+  const ascSortedLevels = levels.toSorted((a, b) => a - b)
+  const descSortedLevels = ascSortedLevels.toReversed()
+  return isEqual(levels, ascSortedLevels) || isEqual(levels, descSortedLevels)
+}
+
+function areSafeLevelDiffs(levels: number[]): boolean {
+  // Check the levels increment by at least one and at most three
+  let prevValue = 0
+  let safe = true
+  levels.forEach((value, index) => {
+    if (index === 0) {
+      prevValue = value
+    } else {
+      const diff = Math.abs(value - prevValue)
+      if (diff < 1 || diff > 3) {
+        safe = false
+      }
+      prevValue = value
+    }
+  })
+  return safe
+}
+
+function isSafeReport(report: string): boolean {
+  const levels = extractReportLevels(report)
+  return areLevelsSorted(levels) && areSafeLevelDiffs(levels)
+}
+
 export function answer() {
   let total = 0
 
-  console.log(`${data.split('\n').length} lines to process`)
-
   data.split('\n').forEach((report) => {
-    const levels = report.split(' ').map((level) => parseInt(level, 10))
-    const ascSortedLevels = levels.toSorted((a, b) => a - b)
-    const descSortedLevels = ascSortedLevels.toReversed()
-    if (isEqual(levels, ascSortedLevels) || isEqual(levels, descSortedLevels)) {
-      // Check the levels increment by at least one and at most three
-      let prevValue = 0
-      let include = true
-      ascSortedLevels.forEach((value, index) => {
-        if (index === 0) {
-          prevValue = value
-        } else {
-          const diff = value - prevValue
-          if (diff < 1 || diff > 3) {
-            include = false
-          }
-          prevValue = value
-        }
-      })
-      if (include) {
-        total++
-      }
+    if (isSafeReport(report)) {
+      total++
     }
   })
 
--- src/puzzles/2024-02/puzzle2.ts
+++ src/puzzles/2024-02/puzzle2.ts
@@ -1,44 +1,56 @@
 import { isEqual } from 'lodash'
 import data from './input.txt?raw'
 
+function extractReportLevels(report: string): number[] {
+  return report.split(' ').map((level) => parseInt(level, 10))
+}
+
+function areLevelsSorted(levels: number[]): boolean {
+  const ascSortedLevels = levels.toSorted((a, b) => a - b)
+  const descSortedLevels = ascSortedLevels.toReversed()
+  return isEqual(levels, ascSortedLevels) || isEqual(levels, descSortedLevels)
+}
+
+function areSafeLevelDiffs(levels: number[]): boolean {
+  // Check the levels increment by at least one and at most three
+  let prevValue = 0
+  let safe = true
+  levels.forEach((value, index) => {
+    if (index === 0) {
+      prevValue = value
+    } else {
+      const diff = Math.abs(value - prevValue)
+      if (diff < 1 || diff > 3) {
+        safe = false
+      }
+      prevValue = value
+    }
+  })
+  return safe
+}
+
+function isSafeReport(report: string): boolean {
+  const levels = extractReportLevels(report)
+  if (areLevelsSorted(levels) && areSafeLevelDiffs(levels)) {
+    return true
+  }
+  // Recheck each report each time for where one level is removed
+  for (let i = 0; i < levels.length; i++) {
+    // Exclude item at index i of array
+    const splicedLevels = levels.toSpliced(i, 1)
+    if (areLevelsSorted(splicedLevels) && areSafeLevelDiffs(splicedLevels)) {
+      return true
+    }
+  }
+  return false
+}
+
 export function answer() {
   let total = 0
 
-  console.log(`${data.split('\n').length} lines to process`)
-
   data.split('\n').forEach((report) => {
-    const levels = report.split(' ').map((level) => parseInt(level, 10))
-
-    for (let i = -1; i < levels.length; i++) {
-      // Exclude item at index i of array
-      const sliceLevels = i === -1 ? levels : levels.toSpliced(i, 1)
-
-      const ascSortedLevels = sliceLevels.toSorted((a, b) => a - b)
-      const descSortedLevels = ascSortedLevels.toReversed()
-      if (
-        isEqual(sliceLevels, ascSortedLevels) ||
-        isEqual(sliceLevels, descSortedLevels)
-      ) {
-        // Check the levels increment by at least one and at most three
-        let prevValue = 0
-        let include = true
-        sliceLevels.forEach((value, index) => {
-          if (index === 0) {
-            prevValue = value
-          } else {
-            const diff = Math.abs(value - prevValue)
-            if (diff < 1 || diff > 3) {
-              include = false
-            }
-            prevValue = value
-          }
-        })
-        if (include) {
-          total++
-          i = levels.length + 1
-          console.log(JSON.stringify(sliceLevels))
-        }
-      }
+    if (isSafeReport(report)) {
+      total++
     }
   })
 
