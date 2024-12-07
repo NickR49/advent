53efa6162326000686ede7605e7ca89ac9fcce59
Nick Rountree <theforestgardener@gmail.com>
2024-12-08 10:44:37 +1300

    Puzzle 2024-07b

--- src/puzzles/2024-07/puzzle2.ts
+++ src/puzzles/2024-07/puzzle2.ts
@@ -2,15 +2,63 @@ import data from './input.txt?raw'
 import moduleText from './puzzle2.ts?raw'
 export { moduleText }
 
+function calculate(values: number[], operations: string): number {
+  let total = values[0]
+  for (let index = 0; index < operations.length; index++) {
+    const nextOperation = operations.slice(index, index + 1)
+    const nextValue = values[index + 1]
+    if (nextOperation === '0') {
+      total += nextValue
+    } else if (nextOperation === '1') {
+      total *= nextValue
+    } else {
+      total = parseInt(`${total}${nextValue}`)
+    }
+  }
+  return total
+}
+
+function isValidLine(total: number, values: number[]): boolean {
+  // Use a base 3 number to iterate through combinations
+  // ie. 3 values => 2 operators => 111 => 8 - 1 => (2 ^ 3) - 1
+  const operationsCount = values.length - 1
+  let base3Counter = Math.pow(3, operationsCount) - 1
+
+  while (base3Counter >= 0) {
+    const operations = Number(base3Counter)
+      .toString(3)
+      .padStart(operationsCount, '0')
+    const calculatedTotal = calculate(values, operations)
+    if (calculatedTotal === total) {
+      return true
+    }
+    base3Counter--
+  }
+
+  return false
+}
+
 export function answer() {
   let total = 0
 
   try {
-    console.log(`${data.split('\n').length} lines to process`)
+    const lines = data.split('\n')
+    lines.forEach((line) => {
+      const lineTotal = parseInt(line.split(':')[0])
+      const lineValues = line
+        .split(':')[1]
+        .trim()
+        .split(/\s+/)
+        .map((value) => parseInt(value))
+
+      if (isValidLine(lineTotal, lineValues)) {
+        total += lineTotal
+      }
+    })
   } catch (e) {
     console.log(`Error: ${e}`)
   }
 
-  //
+  // 271691107779347
   return total
 }
