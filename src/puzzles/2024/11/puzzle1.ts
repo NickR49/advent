1ff23194abc4bc30203c374844e0bcccfafc1e7e
Nick Rountree <theforestgardener@gmail.com>
2024-12-11 19:16:04 +1300

    Puzzle 2024-11

---
+++ src/puzzles/2024/11/puzzle1.ts
@@ -0,0 +1,42 @@
+import { log } from '~/utils/log'
+import moduleText from './puzzle1.ts?raw'
+export { moduleText }
+
+function splitNum(value: number): number[] {
+  const str = String(value)
+  return [
+    Number(str.slice(0, str.length / 2)),
+    Number(str.slice(str.length / 2, str.length)),
+  ]
+}
+
+function blink(inputNumbers: number[]): number[] {
+  const outputNumbers: number[] = []
+  inputNumbers.forEach((number) => {
+    if (number === 0) {
+      outputNumbers.push(1)
+    } else if (String(number).length % 2 === 0) {
+      outputNumbers.push(...splitNum(number))
+    } else {
+      outputNumbers.push(number * 2024)
+    }
+  })
+  return outputNumbers
+}
+
+export function answer() {
+  // let line = '125 17'
+  let line = '8069 87014 98 809367 525 0 9494914 5'
+  let numbers = line.split(' ').map(Number)
+  try {
+    for (let i = 0; i < 25; i++) {
+      numbers = blink(numbers)
+    }
+  } catch (e) {
+    log(`Error: ${e}`)
+  }
+
+  return numbers.length
+}
+
+export const confirmedAnswer = 183484
---
+++ src/puzzles/2024/11/puzzle2.ts
@@ -0,0 +1,51 @@
+import { log } from '~/utils/log'
+import moduleText from './puzzle2.ts?raw'
+export { moduleText }
+
+function splitNum(value: number): number[] {
+  const str = String(value)
+  return [
+    Number(str.slice(0, str.length / 2)),
+    Number(str.slice(str.length / 2, str.length)),
+  ]
+}
+
+function blink(inputNumbers: Map<number, number>): Map<number, number> {
+  const outputNumbers = new Map<number, number>()
+  inputNumbers.forEach((count, number) => {
+    if (number === 0) {
+      outputNumbers.set(1, (outputNumbers.get(1) || 0) + count)
+    } else if (String(number).length % 2 === 0) {
+      const [a, b] = splitNum(number)
+      outputNumbers.set(a, (outputNumbers.get(a) || 0) + count)
+      outputNumbers.set(b, (outputNumbers.get(b) || 0) + count)
+    } else {
+      outputNumbers.set(
+        number * 2024,
+        (outputNumbers.get(number * 2024) || 0) + count,
+      )
+    }
+  })
+  return outputNumbers
+}
+
+export function answer() {
+  // let line = '125 17' // sample
+  let line = '8069 87014 98 809367 525 0 9494914 5'
+  let numbers = line.split(' ').map(Number)
+
+  // Keep a track of many times each number occurs
+  let numberMap = new Map<number, number>(numbers.map((n) => [n, 1]))
+
+  try {
+    for (let i = 0; i < 75; i++) {
+      numberMap = blink(numberMap)
+    }
+  } catch (e) {
+    log(`Error: ${e}`)
+  }
+
+  return Array.from(numberMap.values()).reduce((acc, count) => acc + count, 0)
+}
+
+export const confirmedAnswer = 218817038947400
