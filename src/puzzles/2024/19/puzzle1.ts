9c11ae676ecf31f30a4ea890783700a6dd786c72
Nick Rountree <theforestgardener@gmail.com>
2024-12-20 06:58:16 +1300

    Puzzle 2024-19

---
+++ src/puzzles/2024/19/puzzle1.ts
@@ -0,0 +1,69 @@
+import { log } from '~/utils/log'
+import data from './input.txt?raw'
+import moduleText from './puzzle1.ts?raw'
+export { moduleText }
+
+const patterns = data.split('\n\n')[0].split(', ')
+const towelsToMake = data.split('\n\n')[1].split('\n')
+
+// TIL - dynamic programming
+function canMakeTowel1(towelToMake: string, patterns: string[]): boolean {
+  const dp: boolean[] = Array(towelToMake.length + 1).fill(false)
+  dp[0] = true // Empty string gets a free pass
+
+  for (let i = 1; i <= towelToMake.length; i++) {
+    for (const pattern of patterns) {
+      const patternFits = i >= pattern.length
+      const prefixCanBeMatched = dp[i - pattern.length]
+      const patternMatches =
+        towelToMake.substring(i - pattern.length, i) === pattern
+      if (patternFits && prefixCanBeMatched && patternMatches) {
+        dp[i] = true
+        // break
+      }
+    }
+  }
+
+  return dp[towelToMake.length]
+}
+
+function canMakeTowel2(towelToMake: string, patterns: string[]): boolean {
+  const dp: boolean[] = new Array(towelToMake.length + 1).fill(false)
+  dp[0] = true // Empty string gets a free pass
+  for (let i = 0; i <= towelToMake.length; i++) {
+    if (dp[i]) {
+      for (const pattern of patterns) {
+        const patternFits = i + pattern.length <= towelToMake.length
+        const patternMatches =
+          towelToMake.slice(i, i + pattern.length) === pattern
+        if (patternFits && patternMatches) {
+          dp[i + pattern.length] = true
+        }
+      }
+    }
+  }
+
+  return dp[towelToMake.length]
+}
+
+export function answer() {
+  const startTime = new Date().getTime()
+  let total = 0
+
+  try {
+    towelsToMake.forEach((towel) => {
+      if (canMakeTowel1(towel, patterns)) {
+        total++
+      }
+    })
+  } catch (e: any) {
+    log(`Error: ${e.message}`)
+    log(`Stack Trace:\n${e.stack}`)
+  }
+
+  const endTime = new Date().getTime()
+  log(`Time to run: ${endTime - startTime}ms`)
+  return total
+}
+
+export const confirmedAnswer = 347
---
+++ src/puzzles/2024/19/puzzle2.ts
@@ -0,0 +1,51 @@
+import { log } from '~/utils/log'
+import data from './input.txt?raw'
+import moduleText from './puzzle2.ts?raw'
+export { moduleText }
+
+const patterns = data.split('\n\n')[0].split(', ')
+const towelsToMake = data.split('\n\n')[1].split('\n')
+
+const findPatternCombinations = (
+  towelToMake: string,
+  patterns: string[],
+): number => {
+  const dp: number[] = new Array(towelToMake.length + 1).fill(0)
+  dp[0] = 1 // Empty string gets a free pass
+  for (let i = 0; i <= towelToMake.length; i++) {
+    if (dp[i]) {
+      for (const pattern of patterns) {
+        const patternFits = i + pattern.length <= towelToMake.length
+        const patternMatches =
+          towelToMake.slice(i, i + pattern.length) === pattern
+        if (patternFits && patternMatches) {
+          dp[i + pattern.length] += dp[i]
+        }
+      }
+    }
+  }
+
+  return dp[towelToMake.length]
+}
+
+export function answer() {
+  const startTime = new Date().getTime()
+  let total = 0
+
+  try {
+    towelsToMake.forEach((towel, index) => {
+      log(`Checking towel ${index}`)
+      const combos = findPatternCombinations(towel, patterns)
+      total += combos
+    })
+  } catch (e: any) {
+    log(`Error: ${e.message}`)
+    log(`Stack Trace:\n${e.stack}`)
+  }
+
+  const endTime = new Date().getTime()
+  log(`Time to run: ${endTime - startTime}ms`)
+  return total
+}
+
+export const confirmedAnswer = 919219286602165
